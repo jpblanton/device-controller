d4f1bcac75e13aba9c276d59629ebf7b568e7a6e
James Blanton <jamespblanton@gmail.com>
2023-01-30 15:29:44 -0500

    added logging

--- device-controller.py
+++ device-controller.py
@@ -1,5 +1,6 @@
 import argparse
 import atexit
+import logging
 
 try:
     import tomllib
@@ -9,25 +10,53 @@ except ModuleNotFoundError:
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {"format": "%(asctime)s %(name)-28s %(levelname)-8s %(message)s"}
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "logs/{:%Y-%m-%d}.log".format(datetime.now()),
+            "formatter": "default",
+        },
+    },
+    "root": {"handlers": ["file"], "level": "DEBUG"},
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
 
 def cleanup(client):
     GPIO.cleanup()
     client.loop_stop()
 
+
 def on_message(client, userdata, message):
+    logger.info(message)
     payload = message.payload
-    device_name = '_'.join(message.topic.split('/')[:2])
-    pin = userdata['config'][device_name]['gpio_pin']
-    pub_topic = userdata['config'][device_name]['pub_topic']
-    err_topic = userdata['config'][device_name]['err_topic']
-    if payload == b'True':
-        GPIO.output(pin, GPIO.LOW)
-        client.publish(pub_topic, True)
-    elif payload == b'False':
-        GPIO.output(pin, GPIO.HIGH)
-        client.publish(pub_topic, False)
+    device_name = "_".join(message.topic.split("/")[:2])
+    try:
+        pin = userdata["config"][device_name]["gpio_pin"]
+        pub_topic = userdata["config"][device_name]["pub_topic"]
+        err_topic = userdata["config"][device_name]["err_topic"]
+    except KeyError:
+        client.publish(
+            "general/error/topic", f"Device name not in config: {device_name}"
+        )
     else:
-        client.publish(err_topic, f"Invalid value in payload: {payload}")
+        if payload == b"True":
+            GPIO.output(pin, GPIO.LOW)
+            # consider checking input(pin) to get status
+            client.publish(pub_topic, True)
+        elif payload == b"False":
+            GPIO.output(pin, GPIO.HIGH)
+            client.publish(pub_topic, False)
+        else:
+            client.publish(err_topic, f"Invalid value in payload: {payload}")
 
 
 def mqtt_connect(topics: list[str], host: str, userdata: dict):
@@ -38,21 +67,23 @@ def mqtt_connect(topics: list[str], host: str, userdata: dict):
     return client
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, required=True, help='Hostname or IP of the MQTT broker')
-    parser.add_argument('--config', type=str, default='device-config.toml')
+    parser.add_argument(
+        "--host", type=str, required=True, help="Hostname or IP of the MQTT broker"
+    )
+    parser.add_argument("--config", type=str, default="device-config.toml")
     args = parser.parse_args()
 
-    with open(args.config, 'rb') as f:
+    with open(args.config, "rb") as f:
         config = tomllib.load(f)
-    
-    GPIO.setmode(GPIO.BCM)
-    sub_topics = [config[dev]['sub_topic'] for dev in config]
-    for device in config:
-        GPIO.setup(config[device]['gpio_pin'], GPIO.OUT)
 
-    client_userdata = {'config': config}
+    GPIO.setmode(GPIO.BCM)
+    sub_topics = [config[dev]["sub_topic"] for dev in config]
+    for device in config:
+        GPIO.setup(config[device]["gpio_pin"], GPIO.OUT)
+
+    client_userdata = {"config": config}
     client = mqtt_connect(sub_topics, args.host, client_userdata)
     client.on_message = on_message
     atexit.register(cleanup, client)
